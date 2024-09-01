2eac1231f4e7ae6c3b9c5a8ab35d8600b71cca14
huanmeng-qwq <1871735932@qq.com>
2024-09-01 20:18:27 +0800

    feat: Update KookBC version

--- src/main/java/me/huanmeng/jkook/plugin/mixin/KBClientMixin.java
+++ src/main/java/me/huanmeng/jkook/plugin/mixin/KBClientMixin.java
@@ -2,10 +2,16 @@ package me.huanmeng.jkook.plugin.mixin;
 
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import snw.kookbc.impl.CoreImpl;
 import snw.kookbc.impl.KBCClient;
+import snw.kookbc.impl.command.litecommands.internal.HelpCommand;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * 2023/7/16<br>
@@ -13,18 +19,14 @@ import snw.kookbc.impl.KBCClient;
  *
  * @author huanmeng_qwq
  */
-@Mixin(value = KBCClient.class, remap = false)
+@Mixin(value = KBCClient.class)
 public class KBClientMixin {
     @Shadow
     @Final
     private CoreImpl core;
 
-    /**
-     * @author huanmeng_qwq
-     * @reason 删除默认/help命令
-     */
-    @Overwrite
-    protected void registerHelpCommand() {
-        core.getLogger().info("成功击毙/help命令。");
+    @Inject(method = "registerCommands", at = @At("HEAD"))
+    public void registerCommands(List<Class<?>> commands, CallbackInfo ci) {
+        commands.removeIf(clazz -> Objects.equals(clazz, HelpCommand.class));
     }
 }
